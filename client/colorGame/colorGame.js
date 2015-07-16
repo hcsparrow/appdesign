543a9ec8d64277c8ba8f9c251d437f792f3abcc5
hcsparrow <hc.sparrow@hotmail.com>
2015-07-16 16:51:52 -0400

    changes were made

--- client/colorGame/colorGame.js
+++ client/colorGame/colorGame.js
@@ -2,5 +2,21 @@ Template.colorGame.events({
 
 	"click #colorButton": function(event){
 
+		var name = ["red", "orange", "yellow", "green", "blue", "pink", "purple", "brown", "black"];
+		var color = ["#ff0000", "#ff6600", "#ffff00", "#33cc33", "#0000ff", "#ff33cc", "#990099", "#663300", "#000000"];
+
+		var n = Math.floor(Math.random() * 8);
+		var c = Math.floor(Math.random() * 8);
+
+		var a = name[n];
+		var b = color[c];
+
+		$("#displayColorName").html(a);
+		$("#displayColorName").css("color", b);
+
+		var userInput = $("#exampleInputColor").val();
+
+		<!-- then if statement -->
+
 	}
 })
