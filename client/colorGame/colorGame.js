fdfa98662298bcaeae354cc36bb69651412ec3e2
hcsparrow <hc.sparrow@hotmail.com>
2015-07-17 09:58:07 -0400

    changes

--- client/colorGame/colorGame.js
+++ client/colorGame/colorGame.js
@@ -1,6 +1,7 @@
 var name = ["red", "orange", "yellow", "green", "blue", "pink", "purple", "brown", "black"];
 var color = ["#ff0000", "#ff6600", "#ffff00", "#33cc33", "#0000ff", "#ff33cc", "#990099", "#663300", "#000000"];
 var limit = 15
+var interval;
 
 Template.colorGame.events({
 	"click #colorButton": function(event){
@@ -35,7 +36,14 @@ Template.colorGame.events({
 	}
 })
 
+function something(){
+	//alert();
+}
+
 Template.colorGame.rendered = function(){
+
+		interval = Meteor.setInterval(something, 5000);
+
 		var n = Math.floor(Math.random() * 8);
 		var c = Math.floor(Math.random() * 8);
 
@@ -46,4 +54,5 @@ Template.colorGame.rendered = function(){
 		$("#displayColorName").css("color", b);
 
 		var userInput = $("#exampleInputColor").val();
+
 }
