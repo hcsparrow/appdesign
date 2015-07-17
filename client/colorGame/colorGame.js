535748daa9d91ace58ce59eea8f35f3b96011ebb
hcsparrow <hc.sparrow@hotmail.com>
2015-07-17 09:46:45 -0400

    changes

--- client/colorGame/colorGame.js
+++ client/colorGame/colorGame.js
@@ -1,10 +1,41 @@
-Template.colorGame.events({
+var name = ["red", "orange", "yellow", "green", "blue", "pink", "purple", "brown", "black"];
+var color = ["#ff0000", "#ff6600", "#ffff00", "#33cc33", "#0000ff", "#ff33cc", "#990099", "#663300", "#000000"];
+var limit = 15
 
+Template.colorGame.events({
 	"click #colorButton": function(event){
 
-		var name = ["red", "orange", "yellow", "green", "blue", "pink", "purple", "brown", "black"];
-		var color = ["#ff0000", "#ff6600", "#ffff00", "#33cc33", "#0000ff", "#ff33cc", "#990099", "#663300", "#000000"];
+		if(limit > 0)
+		{
+			var n = Math.floor(Math.random() * 8);
+			var c = Math.floor(Math.random() * 8);
 
+			var a = name[n];
+			var b = color[c];
+
+			$("#displayColorName").html(a);
+			$("#displayColorName").css("color", b);
+
+			var userInput = $("#exampleInputColor").val();
+
+			if(a != userInput)
+			{
+				$("#incorrectMessage").html('Your answer is incorrect. Try again.');
+			}
+			else
+			{
+				limit--;
+			}
+		}
+		else
+		{
+			// game over
+		}
+
+	}
+})
+
+Template.colorGame.rendered = function(){
 		var n = Math.floor(Math.random() * 8);
 		var c = Math.floor(Math.random() * 8);
 
@@ -15,8 +46,4 @@ Template.colorGame.events({
 		$("#displayColorName").css("color", b);
 
 		var userInput = $("#exampleInputColor").val();
-
-		<!-- then if statement -->
-		
-	}
-})
+}
