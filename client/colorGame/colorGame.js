c3b6fcb7c4006ff59ac6aa5de43bdf1de4f1ded2
hcsparrow <hc.sparrow@hotmail.com>
2015-07-17 10:16:19 -0400

    changes

--- client/colorGame/colorGame.js
+++ client/colorGame/colorGame.js
@@ -2,6 +2,9 @@ var name = ["red", "orange", "yellow", "green", "blue", "pink", "purple", "brown
 var color = ["#ff0000", "#ff6600", "#ffff00", "#33cc33", "#0000ff", "#ff33cc", "#990099", "#663300", "#000000"];
 var limit = 15
 var interval;
+var time = 0;
+var firstName;
+var bool = true;
 
 Template.colorGame.events({
 	"click #colorButton": function(event){
@@ -14,43 +17,50 @@ Template.colorGame.events({
 			var a = name[n];
 			var b = color[c];
 
-			$("#displayColorName").html(a);
-			$("#displayColorName").css("color", b);
-
 			var userInput = $("#exampleInputColor").val();
 
-			if(a != userInput)
+			if(firstName != userInput)
 			{
 				$("#incorrectMessage").html('Your answer is incorrect. Try again.');
+
 			}
 			else
 			{
+				$("#incorrectMessage").html('Your answer is correct.');
+				firstName = a;
+				$("#displayColorName").html(a);
+				$("#displayColorName").css("color", b);
+				$("#exampleInputColor").val("");
 				limit--;
 			}
 		}
 		else
 		{
-			// game over
+			bool = false;
+			$("#gameOver").html('You have finished the game. Your time is ' + (time/1000) + " seconds");
 		}
 
 	}
 })
 
-function something(){
-	//alert();
+function braction(){
+	if(bool)
+	{
+		time++;
+	}
 }
 
 Template.colorGame.rendered = function(){
 
-		interval = Meteor.setInterval(something, 5000);
+		interval = Meteor.setInterval(braction, 1);
 
 		var n = Math.floor(Math.random() * 8);
 		var c = Math.floor(Math.random() * 8);
 
-		var a = name[n];
+		firstName = name[n];
 		var b = color[c];
 
-		$("#displayColorName").html(a);
+		$("#displayColorName").html(firstName);
 		$("#displayColorName").css("color", b);
 
 		var userInput = $("#exampleInputColor").val();
